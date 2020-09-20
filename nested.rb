65500580fb839514a2dad2df2bf579d33cf34351
Erick Castille <ecastille924@gmail.com>
2020-09-20 15:14:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,9 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto] = {known_for:"Ruby", languages: ["LISP, "C"] }
+programmer_hash[:yukihiro_matsumoto] = {known_for:"Ruby", languages: ["LISP" , "C"]}
+
+programmer_hash
     
 end
 
@@ -106,8 +108,10 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+    
+     programmer_hash[:alan_kay] = {known_for: "GUI"}
      
-     
+     programmer_hash
 end
 
 def adding_to_dennis
